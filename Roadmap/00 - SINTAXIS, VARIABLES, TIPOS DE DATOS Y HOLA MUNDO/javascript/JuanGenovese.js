020bdcd9a8189c203c47455fb344a01a65c8bbdb
Juan Genovese <juancruzgenovese22@gmail.com>
2025-01-23 18:12:36 -0300

    Mi primer ejercicio

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/JuanGenovese.js
@@ -0,0 +1,31 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+
+/* 
+   EJERCICIO:
+  - Crea un comentario en el código y coloca la URL del sitio web oficial del
+    lenguaje de programación que has seleccionado.
+  - Representa las diferentes sintaxis que existen de crear comentarios
+    en el lenguaje (en una línea, varias...).
+  - Crea una variable (y una constante si el lenguaje lo soporta).
+  - Crea variables representando todos los tipos de datos primitivos
+    del lenguaje (cadenas de texto, enteros, booleanos...).
+  - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+*/
+
+var variable = ""
+const constante = ""
+
+const numero = 1
+const string = "string"
+const booleanos = true
+const nulo = null
+const indefinido = undefined
+const simbolos = Symbol("simbolo")
+const BigInt = BigInt("1234567890123456789012345678901234567890")
+
+console.log("¡Hola, Javascript!")
+
+
+
